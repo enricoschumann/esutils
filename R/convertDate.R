5033c885a222b7f414ac52c726f334658621c3f3
Enrico Schumann <es@enricoschumann.net>
2014-06-30 10:31:24 +0200

    * DESCRIPTION (Version): Version: 0.0-1

--- R/convertDate.R
+++ R/convertDate.R
@@ -5,7 +5,7 @@ convertDate <- function(x, type, fraction = FALSE) {
     } else if (type == "matlab" && !fraction) {
         as.Date(x, origin = "1970-01-01") - 719529
     } else if (type == "matlab" && fraction) {
-        as.POSIXct((x - 719529)*86400, origin = "1970-01-01")
+        as.POSIXct((x - 719529) * 86400, origin = "1970-01-01")
     } else
         stop("unknown type")
 }
