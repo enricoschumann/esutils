d708fd7c5d12ba0a167765c79107e21c86ac5219
Enrico Schumann <es@enricoschumann.net>
2014-08-27 14:39:24 +0200

    see ChangeLog

--- R/nth.R
+++ R/nth.R
@@ -1,5 +1,7 @@
 ## -*- truncate-lines: t; -*-
-## Time-stamp: <2014-07-31 09:44:19 CEST (es)>
+## Time-stamp: <2014-08-27 14:39:02 CEST (es)>
 
-nth <- function (x, n, first = 1L) 
-    x[seq(first, length(x), by = n)]
+nth <- function (x, n, first = 1L)
+    if (is.matrix(x))
+        x[seq(first, nrow(x), by = n), ] else
+            x[seq(first, length(x), by = n)]
