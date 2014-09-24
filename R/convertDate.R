1860f296832a50e8f7d890c0efca90413590ae0e
Enrico Schumann <es@enricoschumann.net>
2014-09-24 18:20:19 +0200

    * DESCRIPTION (Version): Version: 0.1-0
    * R/convertDate.R (convertDate): 'fraction' is now also supported
    for 'excel', and there is a new argument 'tz'

--- R/convertDate.R
+++ R/convertDate.R
@@ -1,11 +1,15 @@
-convertDate <- function(x, type, fraction = FALSE) {
+convertDate <- function(x, type, fraction = FALSE, tz = "") {
     type <- tolower(type)
     if (type == "excel" && !fraction){
-        as.Date(x, origin = "1899-12-30")              
+        as.Date(x, origin = "1899-12-30")
+    } else if (type == "excel") {
+        tmp <- as.POSIXct(x * 86400, origin = "1899-12-30", tz = "UTC")
+        as.POSIXct(strptime(format(tmp), format = "%Y-%m-%d %H:%M:%S", tz = tz))        
     } else if (type == "matlab" && !fraction) {
         as.Date(x, origin = "1970-01-01") - 719529
     } else if (type == "matlab" && fraction) {
-        as.POSIXct((x - 719529) * 86400, origin = "1970-01-01")
+        tmp <- as.POSIXct((x - 719529) * 86400, origin = "1970-01-01")
+        as.POSIXct(strptime(format(tmp), format = "%Y-%m-%d %H:%M:%S", tz = tz))        
     } else
         stop("unknown type")
 }
