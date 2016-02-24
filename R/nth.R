1ca4e39000b1b802288b5d4c6d0b7ba2214ef4cf
Enrico Schumann <es@enricoschumann.net>
2016-02-24 14:35:34 +0100

    deleted old files; updated README (sytem requirements)
    - all functions are now collected in one file, 'functions.R'

--- R/nth.R
+++
@@ -1,7 +0,0 @@
-## -*- truncate-lines: t; -*-
-## Time-stamp: <2015-06-17 16:43:01 CEST (es)>
-
-## nth <- function (x, n, first = 1L)
-##     if (is.matrix(x))
-##         x[seq(first, nrow(x), by = n), ] else
-##             x[seq(first, length(x), by = n)]
